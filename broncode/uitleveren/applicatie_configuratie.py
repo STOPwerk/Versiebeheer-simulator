ab4a673528ad0c90f495fb63f0661124d52c92b9
STOPwerk <robijn@good-heavens.nl>
2022-11-06 17:42:04 +0100

    Release 2022-11-06 17:42:04

--- broncode/uitleveren/applicatie_configuratie.py
+++ broncode/uitleveren/applicatie_configuratie.py
@@ -4,7 +4,7 @@
 #
 #======================================================================
 
-Applicatie_versie = "versie d.d. 2022-11-06 17:38:43"
+Applicatie_versie = "versie d.d. 2022-11-06 17:42:04"
 
 STOP_Documentatie_Url = "https://koop.gitlab.io/STOP/voorinzage/standaard-preview-b/"
 STOP_Repository_Url = "https://gitlab.com/koop/STOP/voorinzage/standaard-preview-b/-/tree/master/"
